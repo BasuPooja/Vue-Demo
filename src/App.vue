49f08a6bc1e228862a2ef6fb4edee4b811341b15
Pooja Kumari <basupooja306@gmail.com>
2025-11-21 12:01:24 +0530

    Initial code

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>Hello Vue!</h1>
+
+  <button @click="count++">
+    Count is: {{ count }}
+  </button>
+  
+</template>
+
+<style>
+button {
+  padding: 8px 16px;
+  font-size: 18px;
+  cursor: pointer;
+}
+</style>
