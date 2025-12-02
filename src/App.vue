2513f81d19fee0bcb87c0739e61a99bae69d959d
Pooja Kumari <basupooja306@gmail.com>
2025-12-02 11:43:58 +0530

    Counter application

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,44 @@
-<script setup>
+<!-- <script setup>
 import { ref } from 'vue'
 
 const count = ref(0)
 </script>
 
 <template>
-  <h1>Hello Vue!</h1>
+  <div>
+    <h2>Counter application</h2>
+    <h1>{{ count }}</h1>
+    <button @click="count++">Increase</button>
+  </div>
+</template> -->
 
-  <button @click="count++">
-    Count is: {{ count }}
-  </button>
-  
+
+<template>
+  <div>
+    <h2>Counter application</h2>
+    <h1>{{ count }}</h1>
+    <button @click="count=0" style="margin-inline-start: 10px; height: 25px;">Reset</button>
+    <button @click="count++" style="margin-inline-start: 10px; height: 25px;">Increase</button>
+
+    <div style="margin-inline-start: 10px;" >
+      <div v-if="count<1">
+        <button disabled @click="count--" style="height: 25px;">Decrease</button>
+      </div>
+      <div v-else >
+        <button @click="count--" style="height: 25px;">Decrease</button>
+      </div>
+    </div>
+    
+    
+  </div>
 </template>
 
-<style>
-button {
-  padding: 8px 16px;
-  font-size: 18px;
-  cursor: pointer;
-}
-</style>
+ <script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+console.log("Hello")
 createApp(App).mount('#app')
+
+
